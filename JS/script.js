cde7d2dd34236abbb5677c5815f2c944fd8d4984
Neyywaar <rossiv036@gmail.com>
2024-01-06 10:20:37 +0545

    Fixed Bugs

--- JS/script.js
+++ JS/script.js
@@ -2,25 +2,33 @@ const buttons = document.querySelectorAll("input[type='button']");
         const dispalyInput = document.getElementById("displayInput");
         const opeartion = document.getElementById("operation");
 
-        buttons.forEach(btn => {
-            btn.addEventListener("click", () => {
-                if (btn.value == "=") {
-                    opeartion.innerHTML = dispalyInput.value;
-                    dispalyInput.value = eval(dispalyInput.value);
-                } else if (btn.value == "C") {
-                    opeartion.innerHTML = "";
-                    dispalyInput.value = "";
-                } else if (btn.value == "Del") {
-                    dispalyInput.value = dispalyInput.value.slice(0, -1);
-                } else {
-                    dispalyInput.value += btn.value;
-                }
+        const buttonClickHandler = (btn) => {
+            if (btn.value === "=") {
+                opeartion.innerHTML = dispalyInput.value;
+                dispalyInput.value = eval(dispalyInput.value.replace(/%/g, '/100'));
+            } else if (btn.value === "C") {
+                opeartion.innerHTML = "";
+                dispalyInput.value = "";
+            } else if (btn.value === "Del") {
+                dispalyInput.value = dispalyInput.value.slice(0, -1);
+            } else {
+                dispalyInput.value += btn.value;
+            }
+        };
+
+        const addButtonListeners = (btn) => {
+            btn.addEventListener("click", () => buttonClickHandler(btn));
+            btn.addEventListener("touchstart", (event) => {
+                event.preventDefault();
+                buttonClickHandler(btn);
             });
-        });
+        };
+
+        buttons.forEach(addButtonListeners);
 
         document.addEventListener("keydown", (event) => {
             if (event.key === "Enter") {
                 opeartion.innerHTML = dispalyInput.value;
-                dispalyInput.value = eval(dispalyInput.value);
+                dispalyInput.value = eval(dispalyInput.value.replace(/%/g, '/100'));
             }
         });
